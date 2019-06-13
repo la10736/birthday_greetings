3a44538cb617a05d475160e050e32c61b1f25daf
michele <michele.damico@gmail.com>
2019-06-13 22:43:49 +0200

    Refactored docker commands

--- tests/birthday_greetings_app.rs
+++ tests/birthday_greetings_app.rs
@@ -1,183 +1,162 @@
 use std::path::{Path, PathBuf};
-use std::process::Command;
+use std::process::{Command, Output};
 use temp_testdir::TempDir;
-use std::env::temp_dir;
-use std::fs::{OpenOptions};
+use std::fs::OpenOptions;
 use std::io::Write;
 use chrono::{Datelike, Duration};
 use std::ops::Add;
 use std::error::Error;
-use json;
+use json::{self, JsonValue};
+use std::marker::PhantomData;
 
 static APP_PATH: &'static str = "target/debug/birthday_greetings";
 static SMTP_DOCKER_IMAGE: &'static str = "fake_smtp";
 static SMTP_DOCKER_FILE_DIR: &'static str = "fake_smtp_docker";
 
+struct DockerCommand;
+
+trait ExecuteDockerCommand {
+    fn execute(&mut self, message: &str) -> Result<String, String>;
+
+    fn map(&self, output: Output, info: &str) -> Result<String, String> {
+        if output.status.success() {
+            Ok(String::from_utf8_lossy(&output.stdout).trim().to_owned())
+        } else {
+            eprintln!(r#"{}:
+        out -> {}
+        err -> {}\n"#, info,
+                      String::from_utf8_lossy(&output.stdout),
+                      String::from_utf8_lossy(&output.stderr)
+            );
+            Err(info.to_owned())
+        }
+    }
+}
+
+impl<'a> ExecuteDockerCommand for &'a mut Command {
+    fn execute(&mut self, message: &str) -> Result<String, String> {
+        self
+            .output()
+            .map_err(|e| e.description().to_owned())
+            .and_then(|o| self.map(o, message))
+    }
+}
+
+impl DockerCommand {
+    pub fn start(&self, image: &str) -> Result<String, String> {
+
+        Command::new("docker")
+            .arg("run")
+            .arg("-d")
+            .arg(image)
+            .execute(&format!("On start docker imege {}", image))
+    }
+
+    pub fn stop(&self, id: &str) -> Result<(), String> {
+        Command::new("docker")
+            .arg("stop")
+            .arg(id)
+            .execute(&format!("On stop docker container {}", id))
+            .map(|_| ())
+    }
+
+    pub fn inspect(&self, id: &str) -> Result<JsonValue, String> {
+        Command::new("docker")
+            .arg("inspect")
+            .arg(id)
+            .execute(&format!("On inspect docker container {}", id))
+            .and_then(|s|
+                json::parse(&s)
+                    .map_err(|e| e.description().to_owned())
+            )
+    }
+
+    pub fn logs(&self, id: &str) -> Result<String, String> {
+        Command::new("docker")
+            .arg("logs")
+            .arg(id)
+            .execute(&format!("On get logs docker container {}", id))
+    }
+
+    pub fn image_exists(&self, image: &str) -> Result<bool, String> {
+        Command::new("docker")
+            .arg("image")
+            .arg("inspect")
+            .arg(image)
+            .execute(&format!("On looking for image {}", image))
+            .map(|_| true)
+    }
+
+    fn build<P: AsRef<Path>>(&self, name: &str, path: P) -> Result<(), String> {
+        Command::new("docker")
+            .arg("build")
+            .arg("-t").arg(name)
+            .arg(path.as_ref())
+            .execute(&format!("On build for image {} at {}",
+                                               name, path.as_ref().to_string_lossy()))
+            .map(|_| ())
+    }
+}
+
 trait SmtpServerState {}
+
 struct Created;
+
 struct Initialized;
+
 impl SmtpServerState for Created {}
+
 impl SmtpServerState for Initialized {}
-use std::marker::PhantomData;
-use json::JsonValue;
+
 
 struct SmtpServer<S: SmtpServerState> {
     img_name: String,
     docker_id: Option<String>,
-    phantom: PhantomData<S>
+    phantom: PhantomData<S>,
 }
 
-impl <S: SmtpServerState> Drop for SmtpServer<S> {
+impl<S: SmtpServerState> Drop for SmtpServer<S> {
     fn drop(&mut self) {
         match &self.docker_id {
             Some(id) => {
-                Command::new("docker")
-                    .arg("stop")
-                    .arg(id)
-                    .output()
-                    .map_err(|e| e.description().to_owned())
-                    .and_then(|o|
-                        if o.status.success() {
-                            Ok(())
-                        } else {
-                            eprintln!("On stopping docker imege {} [{}]: \
-                out -> {}\
-                err -> {}", self.img_name, id,
-                                      String::from_utf8_lossy(&o.stdout),
-                                      String::from_utf8_lossy(&o.stderr)
-                            );
-                            Err("Cannot stop docker container".to_owned())
-                        }
-                    );
-
-            },
+                let _ = DockerCommand.stop(id);
+            }
             None => {}
         }
     }
 }
 
 impl SmtpServer<Created> {
-    pub fn new(img_name: &str) -> Self {
+    pub fn new(img_name: &str) -> SmtpServer<Created> {
         Self { img_name: img_name.to_owned(), docker_id: None, phantom: PhantomData }
     }
 
     pub fn start(self) -> Result<SmtpServer<Initialized>, String> {
-        let id = Command::new("docker")
-            .arg("run")
-            .arg("-d") // Detached
-            .arg(&self.img_name)
-            .output()
-            .map_err(|e| e.description().to_owned())
-            .and_then(|o|
-                if o.status.success() {
-                    Ok(String::from_utf8_lossy(&o.stdout).trim().to_owned())
-                } else {
-                    eprintln!("On starting docker imege {}: \
-                out -> {}\
-                err -> {}", self.img_name,
-                              String::from_utf8_lossy(&o.stdout),
-                              String::from_utf8_lossy(&o.stderr)
-                    );
-                    Err("Cannot start docker image".to_owned())
-                }
-            )?;
+        let id = DockerCommand.start(&self.img_name)?;
         Ok(SmtpServer { img_name: self.img_name.clone(), docker_id: Some(id.to_string()), phantom: PhantomData })
     }
 }
 
 impl SmtpServer<Initialized> {
     pub fn address(&self) -> Result<String, String> {
-        let j = self.inspect()?;
+        let j = DockerCommand.inspect(self.id())?;
         Ok(format!("{}:2525", &j[0]["NetworkSettings"]["IPAddress"]))
     }
 
     pub fn logs(&self) -> Result<String, String> {
-        Command::new("docker")
-            .arg("logs")
-            .arg(self.id())
-            .output()
-            .map_err(|e| e.description().to_owned())
-            .and_then(|o|
-                if o.status.success() {
-                    Ok(String::from_utf8_lossy(&o.stdout).trim().to_owned())
-                } else {
-                    eprintln!("On inspecting docker logs {}: \
-                out -> {}\
-                err -> {}", self.id(),
-                              String::from_utf8_lossy(&o.stdout),
-                              String::from_utf8_lossy(&o.stderr)
-                    );
-                    Err("Cannot inspect docker logs".to_owned())
-                }
-            )
+        DockerCommand.logs(self.id())
     }
 
     fn id(&self) -> &str {
         self.docker_id.as_ref().unwrap()
     }
-
-    fn inspect(&self) -> Result<JsonValue, String> {
-        Command::new("docker")
-            .arg("inspect")
-            .arg(self.id())
-            .output()
-            .map_err(|e| e.description().to_owned())
-            .and_then(|o|
-                if o.status.success() {
-                    json::parse(&String::from_utf8_lossy(&o.stdout))
-                        .map_err(|e| e.description().to_owned())
-                } else {
-                    eprintln!("On inspecting docker imege {}: \
-                out -> {}\
-                err -> {}", self.id(),
-                              String::from_utf8_lossy(&o.stdout),
-                              String::from_utf8_lossy(&o.stderr)
-                    );
-                    Err("Cannot inspect docker image".to_owned())
-                }
-            )
-    }
-
-
 }
 
-fn docker_image_exists(name: &str) -> bool {
-    Command::new("docker")
-        .arg("image")
-        .arg("inspect")
-        .arg(name)
-        .output()
-        .map(|o| o.status.success())
-        .unwrap_or(false)
-}
-
-fn docker_image_build<P: AsRef<Path>>(name: &str, path: P) -> Result<(), String> {
-    Command::new("docker")
-        .arg("build")
-        .arg("-t").arg(name)
-        .arg(path.as_ref())
-        .output()
-        .map_err(|e| e.description().to_owned())
-        .and_then(|o|
-            if o.status.success() {
-                Ok(())
-            } else {
-                eprintln!("On building docker imege {}: \
-                out -> {}\
-                err -> {}", path.as_ref().to_string_lossy(),
-                          String::from_utf8_lossy(&o.stdout),
-                          String::from_utf8_lossy(&o.stderr)
-                );
-                Err("Cannot build docker image".to_owned())
-            }
-        )
-}
-
-fn build_smtp_container<P: AsRef<Path>>(name: &str, path: P) -> Result<(),String> {
-    if docker_image_exists(name) {
+fn build_smtp_container<P: AsRef<Path>>(name: &str, path: P) -> Result<(), String> {
+    if DockerCommand.image_exists(name).unwrap_or(false) {
         Ok(())
     } else {
-        docker_image_build(name, path)
+        DockerCommand.build(name, path)
     }
 }
 
@@ -216,12 +195,14 @@ fn should_fail() {
     write!(f, "Paolino, Paperino, {}, paolino.paperino@dmail.com",
            chrono::Local::today()
                .with_year(1920).unwrap()
-               .format("%Y/%m/%d").to_string());
+               .format("%Y/%m/%d").to_string())
+        .expect("Cannot write entry");
     write!(f, "Paperone, De Paperoni, {}, paperon.depaperoni@dmail.com",
            chrono::Local::today()
                .add(Duration::days(1))
                .with_year(1867).unwrap()
-               .format("%Y/%m/%d").to_string());
+               .format("%Y/%m/%d").to_string())
+        .expect("Cannot write entry");
 
 
     let smtp_server = smtp_server();
